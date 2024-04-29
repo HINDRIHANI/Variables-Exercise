b93c6c0dc666b86c9ac1c8936d8706f893281abb
hind1313 <hind1313@hotmail.com>
2024-04-29 12:13:05 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declare and initialize variables
+            string name = "John";
+            int age = 31;
+            char middleInitial = 'D';
+            bool isOver18 = true;
+            double currentTemp = 77.9;
+            decimal currentPrice = 29.99m;
+
+            // Writing the variables in the console
+            Console.WriteLine($"Name:{name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"curent Temp: {currentTemp}");
+            Console.WriteLine($"current Price: {currentPrice}");
         }
     }
 }
